4a447c09eebbcff5115755f198ca71b1d695d616
Akbangar47 <99977823+Akbangar47@users.noreply.github.com>
2022-07-04 19:18:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", function (req, res) {
 })
 
 app.get("/api/main", function (req, res) {
-    res.send("Namaste from api");
+    res.send("Express is a node js web application framework that provides broad features for building web and mobile applications. It is used to build a single page, multipage, and hybrid web application. It's a layer built on the top of the Node js that helps manage servers and routes.");
 })
 
 app.listen(5001);
